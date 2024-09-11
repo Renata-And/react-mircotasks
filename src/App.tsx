7122efbdfa58747e58a096208dd78e0d53e30c6f
renata-and <renata.androsova@gmail.com>
2024-09-11 19:04:09 +0700

    Button component - callBack

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { MouseEvent } from 'react';
 import './App.css';
-import { NewComponent } from './NewComponent';
+import { NewComponent } from './components/NewComponent';
+import { Button } from './components/Button';
 
 function App() {
+  const button1Foo = (subscriber: string, age: number, address: string) => {
+    console.log(subscriber, age, address)
+  }
+
+  const button2Foo = (subscriber: string) => {
+    console.log(subscriber)
+  }
+
+  const button3Foo = () => {
+    console.log('I\'m stupid button')
+  }
+
+  const onClickHandler = (event: MouseEvent<HTMLButtonElement>) => {
+    console.log('Hi');
+    console.log(event);
+
+  }
+
   return (
-    <NewComponent />
+    <div className='App'>
+      {/* <NewComponent /> */}
+      <button onClick={onClickHandler}>Event</button>
+      <Button name={'YouTubeChannel - 1'} callBack={() => button1Foo('Renata', 33, 'Nha Trang')} />
+      <Button name={'YouTubeChannel - 2'} callBack={() => button2Foo('Stepa')} />
+      <Button name={'YouTubeChannel - 3'} callBack={button3Foo} />
+    </div>
   );
 }
 
---
+++ src/components/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+type ButtonType = {
+  name: string
+  callBack: () => void
+}
+
+export const Button = (props: ButtonType) => {
+  const onclickHandler = () => {
+    props.callBack()
+  }
+
+  return (
+    <div>
+      <button onClick={onclickHandler}>{props.name}</button>
+    </div>
+  )
+}
